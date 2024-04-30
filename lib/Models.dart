377a177ddc67880455258a1db3f2a16aa12d6838
GurshanRai <grai@seattleu.edu>
2024-04-29 21:11:21 -0700

    Previous commits didn't have messages, here are following change made in this commit and previous commits, added models.dart (this commit), unit tests, dares and AOK yaml files

---
+++ lib/Models.dart
@@ -0,0 +1,14 @@
+class ActOfKindness {
+  final String act;
+  final int severity;
+
+  ActOfKindness({required this.act, required this.severity});
+}
+
+class Dare {
+  final String dare;
+  final int severity;
+
+  Dare({required this.dare, required this.severity});
+}
+
