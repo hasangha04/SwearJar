98167d788e6ce312b0fad920658db7b300ad6900
GurshanRai <grai@seattleu.edu>
2024-05-13 16:59:03 -0700

    deleted files related to Yaml which aren't needed anymore

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:swear_jar/yaml_reader.dart';
-import 'package:swear_jar/models.dart';
 import 'package:swear_jar/main.dart';
 
 void main() {
@@ -20,27 +18,5 @@ void main() {
     // Verify that our counter has incremented.
     expect(find.text('Swears in Jar: 0'), findsNothing);
     expect(find.text('Swears in Jar: 1'), findsOneWidget);
-  });
-
-  group('DaresYamlReader', () {
-    testWidgets('Reading dares from YAML file', (WidgetTester tester) async {
-      final daresYamlReader = DaresYamlReader();
-      final daresList = await daresYamlReader.readDares('dares.yaml');
-
-      expect(daresList, isNotEmpty);
-      expect(daresList.length, greaterThan(0));
-      expect(daresList[0], isInstanceOf<Dare>());
-    });
-  });
-
-  group('ActsYamlReader', () {
-    testWidgets('Reading acts of kindness from YAML file', (WidgetTester tester) async {
-      final actsYamlReader = ActsYamlReader();
-      final actsList = await actsYamlReader.readActs('AOK.yaml');
-
-      expect(actsList, isNotEmpty);
-      expect(actsList.length, greaterThan(0));
-      expect(actsList[0], isInstanceOf<ActOfKindness>());
-    });
   });
 }
