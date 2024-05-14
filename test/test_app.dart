de8d13b7e9a064f13b925dbe243a10a0ad1d74f1
Harpreet Sangha <hasangha04@gmail.com>
2024-05-13 18:04:48 -0700

    Added tests for sprint 2

--- test/test_app.dart
+++ test/test_app.dart
@@ -1,14 +1,17 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:swear_jar/main.dart';
-import 'package:swear_jar/firebase_service.dart';
-import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
-import 'package:cloud_firestore_mocks/cloud_firestore_mocks.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart';
+
+class MockFirebasePlatform extends Mock implements FirebasePlatform {}
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
   setUpAll(() async {
+    FirebasePlatform.instance = MockFirebasePlatform();
     await Firebase.initializeApp();
   });
 
